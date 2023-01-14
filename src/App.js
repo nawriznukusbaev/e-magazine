e8f5e827a4c7d1797e2f85b54dd05449394e2509
Nawriz Nukusbaev <nawriznukusbaev@gmail.com>
2023-01-14 19:08:01 +0500

    Run project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import './App.css';
-import React from 'react';
-import {Routes, Route} from "react-router-dom";
-import Layout from "./pages/layout";
-import Homepage from "./pages/homepage";
-import NotFound from "./pages/404";
-import Admin from "./pages/admin";
-import LoginForm from "./components/loginForm";
-import Cart from "./components/cart";
-import {RequireAuth} from "./hoc/RequireAuth";
-import {AuthProvider} from "./hoc/AuthProvider";
-import {Provider, useSelector} from "react-redux";
-import state from './store/slices/store';
+import reportWebVitals from './reportWebVitals';
+import React, {useEffect} from 'react';
+import {Route, Routes, Link,} from "react-router-dom";
 
-const App = () => {
-    return (<Provider store={state}>
-        <AuthProvider>
-                <Routes>
-                    <Route path="/" element={<Layout/>}>
-                        <Route index element={<Homepage/>}/>
-                        <Route path="cart" element={<Cart/>}/>
-                        <Route path='*' element={<NotFound/>}/>
-                    </Route>
-                    <Route path="/admin" element={
-                        <RequireAuth>
-                            <Admin/>
-                        </RequireAuth>
-                    }/>
-                    <Route path="/login" element={<LoginForm/>}/>
-                </Routes>
-        </AuthProvider>
-        </Provider>
+
+function App() {
+       return (
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/posts" element={<Posts/>}/>
+                <Route path="/comments" element={<Comments/>}/>
+            </Routes>
     );
-};
-
-export default App
+}
+export default App;
---
+++ src/components/card-home/card.js
@@ -0,0 +1,8 @@
+import '../../App.css';
+function Home(){
+    return (<div className={'Container'}>
+
+    </div>);
+}
+
+export default CardHome;
